34c38ee951b593d09621ebee0009fa2f9645bed9
khuq <ugur_hakan@yahoo.fr>
2022-04-05 17:29:29 +0200

    création de components pour lister les produits au lieu de l'avoir directement sur la page en question

---
+++ src/components/compPizza.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <div v-for="pizzaIdx,idx of foncPizza" :key="idx">
+            <li>{{pizzaIdx.title}} : {{pizzaIdx.prix}} €</li>
+        </div>       
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'compPizza',
+    computed:{
+        foncPizza(){
+            return this.$store.getters.getPizza;
+        }
+    }
+}
+</script>
